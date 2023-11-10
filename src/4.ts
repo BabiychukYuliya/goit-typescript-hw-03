58064bb67d9a7be7700047cd38df92ca7ea6d0e8
BabiychukYuliya <julia4233097@gmail.com>
2023-11-10 16:49:04 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,34 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  public getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  constructor(public tenants: string[]) {}
+  public door: boolean;
+  public key: Key;
+  public comeIn(tenant: string) {
+    if (this.door === true) {
+      this.tenants.push(tenant);
+    }
+  }
+  public abstract OpenDoor(key: Key): void;
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +37,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
